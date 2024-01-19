3f2c7c80aca755672550dc18baf2e6be58b29629
Geoffrey <gmwangi3174@gmail.com>
2024-01-19 13:56:28 +0300

    first commit

---
+++ app/meals/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Meal Not found</h1>
+      <p>The meal you requested could not be found.</p>
+    </main>
+  );
+};
+
+export default NotFound;
---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>The page you requested could not be found.</p>
+    </main>
+  );
+};
+
+export default NotFound;
